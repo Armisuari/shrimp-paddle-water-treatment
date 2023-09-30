dadf0c9343ab7e7cfe88755d34033b0a2bbf98d6
Arief Nur Misuari <arief.nur@efishery.com>
2023-10-01 03:06:23 +0700

    feat(main) : implement kalman filter library

--- src/main_app.h
+++ src/main_app.h
@@ -3,6 +3,7 @@
 
 #include "config.h"
 #include "mdns.h"
+#include "ServerHandler.h"
 
 // include sensors library
 #include "Sensors/DS18b20.h"
@@ -10,6 +11,14 @@
 #include "Sensors/ECSensor.h"
 #include "Sensors/pHSensor.h"
 #include "Sensors/TurbSensor.h"
+
+#define KALMAN_FILTER
+
+// for smoothie reading purposes
+#ifdef KALMAN_FILTER
+#include <SimpleKalmanFilter.h>
+SimpleKalmanFilter kFilter(2, 2, 0.01);
+#endif
 
 // mdns setup
 const char *version = CONFIG_MAIN_FW_VERSION_STRING;
@@ -20,6 +29,7 @@ void setupMDNSResponder(char *hostname);
 
 // connectivity
 WifiHandler wifi(CONFIG_MAIN_WIFI_DEFAULT_SSID, CONFIG_MAIN_WIFI_DEFAULT_PASS);
+ServerHandler server("myServer");
 
 // sensor object
 DS18B20 tempSensor(CONFIG_SENSOR_TEMPERATURE_PIN);
@@ -33,14 +43,20 @@ Temperature_t tempValue;
 const char *ntpServer = "pool.ntp.org";
 
 void printSensorValue(Temperature_t temp);
+void printToPlot(Temperature_t temp);
 
 void setup()
 {
     Serial.begin(115200);
     generateClientID(clientID); // Generate clientID based on MAC Addr
 
-    // Wifi setup
+    // connectivity setup
     wifi.init();
+
+    // NTP time sync
+    configTime(0, 0, ntpServer);
+
+    server.begin();
 
     // MDNS
     setupMDNSResponder(clientID);
@@ -49,19 +65,28 @@ void setup()
     configTime(0, 0, ntpServer);
     delay(2000);
 
+    // DS18B20::getValue();
+
     tempSensor.begin();
 }
 
 void loop()
 {
+    // vTaskDelete(NULL);
+
     // parameter value object
     Temperature_t tempValue;
 
     tempSensor.measure(tempValue);
 
-    printSensorValue(tempValue);
-    
-    vTaskDelay(5000);
+#ifdef KALMAN_FILTER
+    Temperature_t filTemp;
+    tempSensor.filter(kFilter.updateEstimate(tempValue.temp), filTemp);
+#endif
+
+    // printSensorValue(tempValue);
+    printToPlot(tempValue);
+    vTaskDelay(1000);
 }
 
 void generateClientID(char *idBuff)
@@ -73,9 +98,20 @@ void generateClientID(char *idBuff)
     sprintf(idBuff, "%s%02X%02X%02X", clientIdPrefix, mac[3], mac[4], mac[5]);
 }
 
-void printSensorValue(Temperature_t temp)
+void printSensorValue(Temperature_t value)
 {
-    Serial.printf("Temperatur : %4.2f ℃\n", temp.temp);
+    Serial.println(" -----------------------");
+    Serial.printf("| Temperature | %4.2f ℃ |\n", value.temp);
+    Serial.println(" -----------------------");
+}
+
+void printToPlot(Temperature_t value)
+{
+    Serial.print("temp:");
+    Serial.print(value.temp);
+    Serial.print(",");
+
+    Serial.println();
 }
 
 void setupMDNSResponder(char *hostname)
